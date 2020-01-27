0d1f9eaf5154885023ff2acf732430a1669d3e50
punith kumar kakkera <punith@puniths-MacBook-Pro.local>
2020-01-26 21:20:37 -0600

    Updates

---
+++ precache-manifest.dc1fb55d0609a1ebbea7bf89c9318e88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "218091514c888d2c0d2e01cbafefdfcc",
+    "url": "/react-redux-app/index.html"
+  },
+  {
+    "revision": "46d59f5cf4449939f607",
+    "url": "/react-redux-app/static/css/main.a352cb39.chunk.css"
+  },
+  {
+    "revision": "c6e1c0f04663ede1ed02",
+    "url": "/react-redux-app/static/js/2.c5fd0816.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-redux-app/static/js/2.c5fd0816.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46d59f5cf4449939f607",
+    "url": "/react-redux-app/static/js/main.c665fcf6.chunk.js"
+  },
+  {
+    "revision": "d731d5cbce6a57004021",
+    "url": "/react-redux-app/static/js/runtime-main.a562af76.js"
+  }
+]);
